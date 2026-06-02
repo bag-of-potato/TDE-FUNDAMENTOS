66991d95b87c6f2a84133b78307833000cde966e
bag-of-potato <90380994+bag-of-potato@users.noreply.github.com>
2026-06-02 09:55:18 -0300

    Classe Ingrediente

---
+++ Ingrediente.java
@@ -0,0 +1,56 @@
+
+/**
+ * Escreva uma descrição da classe Ingrediente aqui.
+ * 
+ * @author (VASCO) 
+ * @version (06.02.2026)
+ */
+public class Ingrediente
+{
+    //Variaveis de instância
+    private String nome;
+    private String medida;
+    private double quantidade;
+
+    //construtor com paramêtro
+    public Ingrediente(String nome, String medida, double quantidade){
+        this.nome = nome;
+        this.medida = medida;
+        this.quantidade = quantidade;
+    }
+
+    //construtor padrão
+    public Ingrediente(){
+        this.nome = null;
+        this.medida = null;
+        this.quantidade = 0;
+    }
+
+    //metodos de acesso
+    //setters
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public void setMedida(String medida){
+        this.medida = medida;
+    }
+    public void setQuantidade(double quantidade){
+        this.quantidade = quantidade;
+    }
+
+    //getters
+    public String getNome(){
+        return this.nome;
+    }
+    public String getMedida(){
+        return this.medida;
+    }
+    public double getQuantidade(){
+        return this.quantidade;
+    }
+    
+    //toString
+    public String toString(){
+        return "Nome: " + this.nome + " Medida: " + this.medida + " Quantidade: " + this.quantidade;
+    }
+}
